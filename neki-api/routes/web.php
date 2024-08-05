e5baadc969e624c8af8f3a3eb503dabeaf2be0f7
Jojohn Tabal <tabal.jojohn@gmail.com>
2024-08-06 00:04:01 +0800

    minor fix

--- neki-api/routes/web.php
+++ neki-api/routes/web.php
@@ -1,7 +1,15 @@
 <?php
 
+use App\Http\Middleware\UserMiddleware;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', function () {
+
+Route::get('/register', function () {
     return view('welcome');
+});
+
+
+Route::middleware([UserMiddleware::class])->group(function () {
+    Route::get('/', function () {
+    });
 });
