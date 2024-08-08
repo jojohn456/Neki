dca87c84451a4c074db3bef751908a4731a49eb5
Jojohn Tabal <tabal.jojohn@gmail.com>
2024-08-08 21:10:27 +0800

    ERD

--- neki-api/routes/web.php
+++ neki-api/routes/web.php
@@ -1,15 +1,15 @@
 <?php
 
+use App\Http\Middleware\PublicMiddleware;
 use App\Http\Middleware\UserMiddleware;
 use Illuminate\Support\Facades\Route;
 
-
-Route::get('/register', function () {
-    return view('welcome');
+Route::middleware([PublicMiddleware::class], 'throttle:60,1')->group(function () {
+    Route::get('/register', function () {
+    });
 });
 
-
-Route::middleware([UserMiddleware::class])->group(function () {
+Route::middleware([UserMiddleware::class], 'throttle:60,1')->group(function () {
     Route::get('/', function () {
     });
 });
