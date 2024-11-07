629923b3037c07ada7127f486ae1a592ec51b40e
Jojohn Tabal <tabal.jojohn@gmail.com>
2024-11-07 23:29:16 +0800

    app

---
+++ neki-app/src/router/routes.ts
@@ -0,0 +1,17 @@
+import { RouteRecordRaw } from 'vue-router';
+
+const routes: RouteRecordRaw[] = [
+  {
+    path: '/',
+    component: () => import('layouts/MainLayout.vue'),
+    children: [{ path: '', component: () => import('../views/IndexPage.vue') }],
+  },
+  // Always leave this as last one,
+  // but you can also remove it
+  {
+    path: '/:catchAll(.*)*',
+    component: () => import('../views/ErrorNotFound.vue'),
+  },
+];
+
+export default routes;
---
+++ neki-app/src/views/IndexPage.vue
@@ -0,0 +1,14 @@
+<template>
+  <q-page>
+    <Navbar />
+    <div class="row">
+      <div class="col-12"></div>
+    </div>
+  </q-page>
+</template>
+
+<script setup lang="ts">
+defineOptions({
+  name: 'IndexPage',
+});
+</script>
