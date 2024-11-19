3394598d3714485de4741bb5b3c056d8fd7d8e7c
Jojohn Tabal <tabal.jojohn@gmail.com>
2024-11-19 19:45:46 +0800

    signin and signup pages

--- neki-app/src/router/routes.ts
+++ neki-app/src/router/routes.ts
@@ -6,6 +6,13 @@ const routes: RouteRecordRaw[] = [
     component: () => import('layouts/MainLayout.vue'),
     children: [{ path: '', component: () => import('../views/IndexPage.vue') }],
   },
+  {
+    path: '/signup',
+    component: () => import('layouts/MainLayout.vue'),
+    children: [
+      { path: '', component: () => import('../views/SignupPage.vue') },
+    ],
+  },
   // Always leave this as last one,
   // but you can also remove it
   {
