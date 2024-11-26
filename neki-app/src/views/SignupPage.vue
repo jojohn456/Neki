df6a7c89bbf1db246be0e7684eaee59752584d65
Jojohn Tabal <tabal.jojohn@gmail.com>
2024-11-27 00:33:35 +0800

    minor fix

--- neki-app/src/views/SignupPage.vue
+++ neki-app/src/views/SignupPage.vue
@@ -1,128 +1,139 @@
 <template>
   <q-page>
-    <Navbar />
     <div class="row">
       <div class="col-12 flex justify-center items-center">
         <div class="card bg-base-100 max-w-[500px] w-full shadow-xl mt-[10%]">
           <div class="card-body">
             <h2 class="card-title justify-center" align="center">SignUp</h2>
-            <label class="input input-bordered flex items-center gap-2">
-              <svg
-                xmlns="http://www.w3.org/2000/svg"
-                viewBox="0 0 16 16"
-                fill="currentColor"
-                class="h-4 w-4 opacity-70"
-              >
-                <path
-                  d="M8 8a3 3 0 1 0 0-6 3 3 0 0 0 0 6ZM12.735 14c.618 0 1.093-.561.872-1.139a6.002 6.002 0 0 0-11.215 0c-.22.578.254 1.139.872 1.139h9.47Z"
-                />
-              </svg>
-              <input
-                v-model="data.firstname"
-                type="text"
-                class="grow"
-                placeholder="FirstName"
-              />
-            </label>
-            <label class="input input-bordered flex items-center gap-2">
-              <svg
-                xmlns="http://www.w3.org/2000/svg"
-                viewBox="0 0 16 16"
-                fill="currentColor"
-                class="h-4 w-4 opacity-70"
-              >
-                <path
-                  d="M8 8a3 3 0 1 0 0-6 3 3 0 0 0 0 6ZM12.735 14c.618 0 1.093-.561.872-1.139a6.002 6.002 0 0 0-11.215 0c-.22.578.254 1.139.872 1.139h9.47Z"
-                />
-              </svg>
-              <input
-                v-model="data.lastname"
-                type="text"
-                class="grow"
-                placeholder="LastName"
-              />
-            </label>
-            <label class="input input-bordered flex items-center gap-2">
-              <svg
-                xmlns="http://www.w3.org/2000/svg"
-                viewBox="0 0 16 16"
-                fill="currentColor"
-                class="h-4 w-4 opacity-70"
-              >
-                <path
-                  d="M2.5 3A1.5 1.5 0 0 0 1 4.5v.793c.026.009.051.02.076.032L7.674 8.51c.206.1.446.1.652 0l6.598-3.185A.755.755 0 0 1 15 5.293V4.5A1.5 1.5 0 0 0 13.5 3h-11Z"
-                />
-                <path
-                  d="M15 6.954 8.978 9.86a2.25 2.25 0 0 1-1.956 0L1 6.954V11.5A1.5 1.5 0 0 0 2.5 13h11a1.5 1.5 0 0 0 1.5-1.5V6.954Z"
-                />
-              </svg>
-              <input
-                v-model="data.email"
-                type="text"
-                class="grow"
-                placeholder="Email"
-              />
-            </label>
-            <label class="input input-bordered flex items-center gap-2">
-              <svg
-                xmlns="http://www.w3.org/2000/svg"
-                viewBox="0 0 16 16"
-                fill="currentColor"
-                class="h-4 w-4 opacity-70"
-              >
-                <path
-                  d="M8 8a3 3 0 1 0 0-6 3 3 0 0 0 0 6ZM12.735 14c.618 0 1.093-.561.872-1.139a6.002 6.002 0 0 0-11.215 0c-.22.578.254 1.139.872 1.139h9.47Z"
-                />
-              </svg>
-              <input type="text" class="grow" placeholder="Username" />
-            </label>
-            <label class="input input-bordered flex items-center gap-2">
-              <svg
-                xmlns="http://www.w3.org/2000/svg"
-                viewBox="0 0 16 16"
-                fill="currentColor"
-                class="h-4 w-4 opacity-70"
-              >
-                <path
-                  fill-rule="evenodd"
-                  d="M14 6a4 4 0 0 1-4.899 3.899l-1.955 1.955a.5.5 0 0 1-.353.146H5v1.5a.5.5 0 0 1-.5.5h-2a.5.5 0 0 1-.5-.5v-2.293a.5.5 0 0 1 .146-.353l3.955-3.955A4 4 0 1 1 14 6Zm-4-2a.75.75 0 0 0 0 1.5.5.5 0 0 1 .5.5.75.75 0 0 0 1.5 0 2 2 0 0 0-2-2Z"
-                  clip-rule="evenodd"
-                />
-              </svg>
-              <input
-                v-model="data.password1"
-                type="password"
-                class="grow"
-                placeholder="Password"
-              />
-            </label>
-            <label class="input input-bordered flex items-center gap-2">
-              <svg
-                xmlns="http://www.w3.org/2000/svg"
-                viewBox="0 0 16 16"
-                fill="currentColor"
-                class="h-4 w-4 opacity-70"
-              >
-                <path
-                  fill-rule="evenodd"
-                  d="M14 6a4 4 0 0 1-4.899 3.899l-1.955 1.955a.5.5 0 0 1-.353.146H5v1.5a.5.5 0 0 1-.5.5h-2a.5.5 0 0 1-.5-.5v-2.293a.5.5 0 0 1 .146-.353l3.955-3.955A4 4 0 1 1 14 6Zm-4-2a.75.75 0 0 0 0 1.5.5.5 0 0 1 .5.5.75.75 0 0 0 1.5 0 2 2 0 0 0-2-2Z"
-                  clip-rule="evenodd"
-                />
-              </svg>
-              <input
-                v-model="data.password2"
-                type="password"
-                class="grow"
-                placeholder="Re-Enter Password"
-              />
-            </label>
-            <button class="btn btn-primary w-full">Sign-Up</button>
-            <small
-              class="flex justify-center"
-              id="link"
-              v-on:click="$router.push('/')"
-              >Back to SignIn</small
+            <Form
+              @submit="submitUserData"
+              :validation-schema="schema"
+              v-slot="{ errors }"
             >
+              <label class="input input-bordered flex items-center gap-2">
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  viewBox="0 0 16 16"
+                  fill="currentColor"
+                  class="h-4 w-4 opacity-70"
+                >
+                  <path
+                    d="M8 8a3 3 0 1 0 0-6 3 3 0 0 0 0 6ZM12.735 14c.618 0 1.093-.561.872-1.139a6.002 6.002 0 0 0-11.215 0c-.22.578.254 1.139.872 1.139h9.47Z"
+                  />
+                </svg>
+                <input
+                  v-model="data.firstname"
+                  type="text"
+                  class="grow"
+                  placeholder="FirstName"
+                />
+              </label>
+              <span>{{ errors.firstname }}</span>
+              <label class="input input-bordered flex items-center gap-2">
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  viewBox="0 0 16 16"
+                  fill="currentColor"
+                  class="h-4 w-4 opacity-70"
+                >
+                  <path
+                    d="M8 8a3 3 0 1 0 0-6 3 3 0 0 0 0 6ZM12.735 14c.618 0 1.093-.561.872-1.139a6.002 6.002 0 0 0-11.215 0c-.22.578.254 1.139.872 1.139h9.47Z"
+                  />
+                </svg>
+                <input
+                  v-model="data.lastname"
+                  type="text"
+                  class="grow"
+                  placeholder="LastName"
+                />
+              </label>
+              <label class="input input-bordered flex items-center gap-2">
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  viewBox="0 0 16 16"
+                  fill="currentColor"
+                  class="h-4 w-4 opacity-70"
+                >
+                  <path
+                    d="M2.5 3A1.5 1.5 0 0 0 1 4.5v.793c.026.009.051.02.076.032L7.674 8.51c.206.1.446.1.652 0l6.598-3.185A.755.755 0 0 1 15 5.293V4.5A1.5 1.5 0 0 0 13.5 3h-11Z"
+                  />
+                  <path
+                    d="M15 6.954 8.978 9.86a2.25 2.25 0 0 1-1.956 0L1 6.954V11.5A1.5 1.5 0 0 0 2.5 13h11a1.5 1.5 0 0 0 1.5-1.5V6.954Z"
+                  />
+                </svg>
+                <input
+                  v-model="data.email"
+                  type="text"
+                  class="grow"
+                  placeholder="Email"
+                />
+              </label>
+              <label class="input input-bordered flex items-center gap-2">
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  viewBox="0 0 16 16"
+                  fill="currentColor"
+                  class="h-4 w-4 opacity-70"
+                >
+                  <path
+                    d="M8 8a3 3 0 1 0 0-6 3 3 0 0 0 0 6ZM12.735 14c.618 0 1.093-.561.872-1.139a6.002 6.002 0 0 0-11.215 0c-.22.578.254 1.139.872 1.139h9.47Z"
+                  />
+                </svg>
+                <input type="text" class="grow" placeholder="Username" />
+              </label>
+              <label class="input input-bordered flex items-center gap-2">
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  viewBox="0 0 16 16"
+                  fill="currentColor"
+                  class="h-4 w-4 opacity-70"
+                >
+                  <path
+                    fill-rule="evenodd"
+                    d="M14 6a4 4 0 0 1-4.899 3.899l-1.955 1.955a.5.5 0 0 1-.353.146H5v1.5a.5.5 0 0 1-.5.5h-2a.5.5 0 0 1-.5-.5v-2.293a.5.5 0 0 1 .146-.353l3.955-3.955A4 4 0 1 1 14 6Zm-4-2a.75.75 0 0 0 0 1.5.5.5 0 0 1 .5.5.75.75 0 0 0 1.5 0 2 2 0 0 0-2-2Z"
+                    clip-rule="evenodd"
+                  />
+                </svg>
+                <input
+                  v-model="data.password1"
+                  type="password"
+                  class="grow"
+                  placeholder="Password"
+                />
+              </label>
+              <label class="input input-bordered flex items-center gap-2">
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  viewBox="0 0 16 16"
+                  fill="currentColor"
+                  class="h-4 w-4 opacity-70"
+                >
+                  <path
+                    fill-rule="evenodd"
+                    d="M14 6a4 4 0 0 1-4.899 3.899l-1.955 1.955a.5.5 0 0 1-.353.146H5v1.5a.5.5 0 0 1-.5.5h-2a.5.5 0 0 1-.5-.5v-2.293a.5.5 0 0 1 .146-.353l3.955-3.955A4 4 0 1 1 14 6Zm-4-2a.75.75 0 0 0 0 1.5.5.5 0 0 1 .5.5.75.75 0 0 0 1.5 0 2 2 0 0 0-2-2Z"
+                    clip-rule="evenodd"
+                  />
+                </svg>
+                <input
+                  v-model="data.password2"
+                  type="password"
+                  class="grow"
+                  placeholder="Re-Enter Password"
+                />
+              </label>
+              <button
+                class="btn btn-primary w-full"
+                v-on:click="submitUserData"
+              >
+                Sign-Up
+              </button>
+              <small
+                class="flex justify-center"
+                id="link"
+                v-on:click="$router.push('/')"
+                >Back to SignIn</small
+              >
+            </Form>
           </div>
         </div>
       </div>
@@ -132,15 +143,26 @@
 
 <script setup lang="ts">
 import { reactive } from 'vue';
-import { useVuelidate } from '@vuelidate/core';
-import { helpers, required } from '@vuelidate/validators';
+import { defineRule } from 'vee-validate';
 
 defineOptions({
   name: 'SignupPage',
 });
 
+const schema = {
+  email: 'required|email',
+  password: 'required|minLength:8',
+};
+
+defineRule('required', (value: any) => {
+  if (!value || !value.length) {
+    return 'This field is required';
+  }
+  return true;
+});
+
 const data = reactive({
-  firstname: 'asdasd',
+  firstname: '',
   lastname: '',
   email: '',
   username: '',
@@ -148,21 +170,5 @@ const data = reactive({
   password2: '',
 });
 
-const rules = {
-  firstname: [
-    { required: helpers.withMessage('firstname is required', required) },
-  ],
-  lastname: [
-    { required: helpers.withMessage('firstname is required', required) },
-  ],
-};
-const v$ = useVuelidate(rules, data);
-let addBtn = false;
-function submitUserData(): void {
-  v$.value.$validate();
-  if (v$.value.$invalid == true) {
-    return;
-  } else {
-  }
-}
+function submitUserData(): void {}
 </script>
